e37d8f53b6fbcd2f9b566097f456ea3bbde4f532
Alan Peña <alancin7@live.com.mx>
2023-04-06 00:07:21 -0500

    Creacion de ejercicio de examen unidad 1

---
+++ Examen Unidad 1/Examen Unidad 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examen Unidad 1
+//
+//  Created by Alan Peña on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
