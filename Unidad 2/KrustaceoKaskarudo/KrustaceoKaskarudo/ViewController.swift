1aa67f1edf6d6487e6e708397efd912a77664989
Alan Peña <alancin7@live.com.mx>
2023-04-18 10:14:14 -0500

    code: ejercicio 20%

---
+++ Unidad 2/KrustaceoKaskarudo/KrustaceoKaskarudo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KrustaceoKaskarudo
+//
+//  Created by Alan Peña on 18/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
